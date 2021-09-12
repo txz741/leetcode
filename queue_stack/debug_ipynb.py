c3f46a2bc8305c7a40920dcf6b54974119555e9a
txz741 <2310957657@qq.com>
2021-09-12 23:45:48 +0800

    add 962

--- queue_stack/debug_ipynb.py
+++ queue_stack/debug_ipynb.py
@@ -3,34 +3,30 @@ import collections
 
 
 class Solution:
-    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        s = []
-        index = 0
-        flag = 0
-        length = len(asteroids)
-        while index < length and asteroids[index] < 0:
-            s.append(asteroids[index])
-            index += 1
-        if index == length:
-            return asteroids
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        q = []
+        hashmap = dict()
+        length = len(nums)
+        for i, num in enumerate(nums):
+            while q and nums[q[-1]] <= num:
+                j = q.pop()
+                hashmap[j] = i
+            q.append(i)
+        for i in q:
+            hashmap[i] = -1
 
-        for i in range(index, length):
-            if asteroids[i] > 0:
-                s.append(asteroids[i])
+        res = 0
+        for k in hashmap:
+            flag = k
+            while hashmap[k] in hashmap and k != hashmap[k]:
+                k = hashmap[k]
             else:
-                while len(s) > 0 and s[-1] > 0 and s[-1] <= abs(asteroids[i]):  # 栈顶是正的就开创
-                    # x = s.pop()
-                    if s.pop() == abs(asteroids[i]):  # 同归
-                        flag = 1
-                        break
-                if (len(s) == 0 or s[-1] < 0) and flag !=1:  # 判定跳出while的条件 全创完了或栈顶是负的
-                    s.append(asteroids[i])
-                else:  # 新来的没创得过
-                    pass
-                flag = 0
-        return s
+                if k - flag > res:
+                    res = k - flag
+
+        return res
 
 
 SS = Solution()
-res = SS.asteroidCollision([5, 10, -5])
+res = SS.maxWidthRamp([9, 8, 1, 0, 1, 9, 4, 0, 4, 1])
 print(res)
