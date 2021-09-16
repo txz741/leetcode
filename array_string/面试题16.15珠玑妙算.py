7561c5f658b2ac84c6fe7bef41927c95792a2e81
txz741 <2310957657@qq.com>
2021-09-16 23:32:27 +0800

    mianshi1615

--- array_string/面试题16.15珠玑妙算.py
+++ array_string/面试题16.15珠玑妙算.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class Solution:
     def masterMind(self, solution: str, guess: str) -> List[int]:
         res = [0, 0]
@@ -17,14 +15,7 @@ class Solution:
         for n in hashmap1:
             if n in hashmap2:
                 temp = set(hashmap1[n]).intersection(set(hashmap2[n]))
-                if not temp:
-                    res[1] += 1
-                else:
-                    res[0] += len(temp)
+                res[0] += len(temp) # 交集长度即为猜中个数
+                res[1] += min(len(hashmap1[n]), len(hashmap2[n])) - len(temp) # 两个列表取最短且减去猜中个数即为伪猜中个数
 
         return res
-
-
-SS = Solution()
-res = SS.masterMind('RGRB', 'BBBY')
-print(res)
